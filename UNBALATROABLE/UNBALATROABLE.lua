80600e80c59884df3e446b1a060d2917514590b2
HeroOfYore <aragornnlink@gmail.com>
2026-01-07 18:40:25 -0800

    new style for the download
    Two inner mods: one for the collabs, and one for joker skins

--- UNBALATROABLE/UNBALATROABLE.lua
+++ UNBALATROABLE/UNBALATROABLE.lua
@@ -129,26 +129,6 @@ SMODS.DeckSkin({
 -----------------------------
 
 
-function SMODS.INIT.JOKERRESKIN()
-    
-    jokers = {
-        j_8_ball = { pos = 0}
-    }
-
-    SMODS.Atlas {
-        key = "8ballable",
-        path = "UNBEATA8BALL.png",
-        px = 71, 
-        py = 95
-    }
-    ---if not UNBALATROABLE_config.disabled then
-        for jkr, data in pairs(jokers) do
-            SMODS["Joker"]:take_ownership(jkr, {atlas = "8ballable",
-            pos = {x = 0 + data.pos, y = 0},
-            soul_pos = {x = 0 + data.pos, y = 1}}, true)
-        end
-    ---end
-end
 
 
 local UNBALATROABLEConfigTab = function()
