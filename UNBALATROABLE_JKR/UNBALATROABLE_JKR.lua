86a2fcc469c78cad227deb723cce94d572c7f12e
HeroOfYore <aragornnlink@gmail.com>
2026-01-10 15:19:40 -0800

    More jokers added
    Mr Bones, Invisible Joker, and Joker Stencil have been reskinned.

--- UNBALATROABLE_JKR/UNBALATROABLE_JKR.lua
+++ UNBALATROABLE_JKR/UNBALATROABLE_JKR.lua
@@ -15,18 +15,21 @@ UNBALATROABLE_JKR.path = mod_path
 function SMODS.INIT.JOKERRESKIN()
     
     jokers = {
-        j_8_ball = { pos = 0}
+        j_8_ball = {pos = 0}
+        j_mr_bones = {pos = 1}
+        j_invisible = {pos = 2}
+        j_stencil = {pos = 3}
     }
 
     SMODS.Atlas {
-        key = "8ballable",
-        path = "UNBEATA8BALL.png",
+        key = "agglomeration",
+        path = "AGGLOMERATION.png",
         px = 71, 
         py = 95
     }
     ---if not UNBALATROABLE_config.disabled then
         for jkr, data in pairs(jokers) do
-            SMODS["Joker"]:take_ownership(jkr, {atlas = "8ballable",
+            SMODS["Joker"]:take_ownership(jkr, {atlas = "agglomeration",
             pos = {x = 0 + data.pos, y = 0},
             soul_pos = {x = 0 + data.pos, y = 1}}, true)
         end
